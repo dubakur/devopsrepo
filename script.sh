3af0709bafdc0d30c936d13d3a503b42cb1872ea
dubakur <52884907+dubakur@users.noreply.github.com>
2024-07-10 10:11:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,22 @@
 
 # script.sh
 
-param1=$1
-param2=$2
+#!/bin/bash
 
-# Perform some operations
-result=$((param1 + param2))
+echo "Hello world!"
+# Assuming apps_in_json_format contains the JSON array
+projects="project1 project2" 
+apps_in_json_format=$(echo $projects| jq -Rc 'split(" ")')
 
-# Print the result (this will be captured by GitHub Actions)
-echo $result
+# Count the number of elements in the JSON array
+num_projects=$(echo "$apps_in_json_format" | jq 'length')
+
+# Print the result
+echo "Number of projects: $num_projects"
+
+if [ "$num_projects" -gt 1 ]; then
+    exit 1
+    echo "something"
+elif [ "$num_projects" -eq 1 ]; then
+    echo $apps_in_json_format
+fi
